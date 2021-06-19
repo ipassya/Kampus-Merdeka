b8f6f7325e3aa296f67363a7c4a87d3766cdae5e
ipassya <46826137+ipassya@users.noreply.github.com>
2021-06-19 17:50:47 +0700

    Update model

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,12 +5,11 @@ class Home extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 		$this->load->model('M_home', 'home');
-		$this->db_oxford = $this->load->database('oxford', TRUE);
 	}
 	
 	public function index() {
-		$upn = $this->home->get();
-		$oxford = $this->db_oxford->query('select * from tb_students')->result_array();
+		$upn = $this->home->getUPN();
+		$oxford = $this->home->getOxford();
 		$data['students'] = array_merge($upn, $oxford);
 		$this->load->view('templates/header');
 		$this->load->view('home/index', $data);
--- application/models/M_home.php
+++ application/models/M_home.php
@@ -2,8 +2,13 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class M_home extends CI_Model {
-	public function get() {
+	public function getUPN() {
 		return $this->db->get('tb_students')->result_array();
+	}
+
+	public function getOxford() {
+		$this->db_oxford = $this->load->database('oxford', TRUE);
+		return $this->db_oxford->get('tb_students')->result_array();
 	}
 
 }
